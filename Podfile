06a659f1f5835c90455eb09f87a3de2f8a8bdef1
JimCampagno <jimcampagno@gmail.com>
2016-04-09 20:15:28 -0400

    Clean up unused files add HTMLReader Pod Prepare for transferring over HTMLReader to swift

---
+++ Podfile
@@ -0,0 +1,9 @@
+use_frameworks!
+
+target 'WhoSaidIt' do
+
+pod 'Firebase', '~> 2.5'
+pod 'HTMLReader', '~> 0.9'
+
+end
+
---
+++ WhoSaidIt/BingSearchAPIClient.swift
@@ -0,0 +1,18 @@
+//
+//  BingSearchAPIClient.swift
+//  WhoSaidIt
+//
+//  Created by Jim Campagno on 4/9/16.
+//  Copyright © 2016 Gamesmith, LLC. All rights reserved.
+//
+
+import Foundation
+import HTMLReader
+
+final class BingSearchAPIClient {
+    
+    
+    
+    
+    
+}
