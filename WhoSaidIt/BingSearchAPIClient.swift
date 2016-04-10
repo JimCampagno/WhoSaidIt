7a4735269c2987bbd4b80a382efaa38abc3b0f3d
JimCampagno <jimcampagno@gmail.com>
2016-04-09 22:26:26 -0400

    Create Bing API Client add various properties

--- WhoSaidIt/BingSearchAPIClient.swift
+++ WhoSaidIt/BingSearchAPIClient.swift
@@ -9,9 +9,53 @@
 import Foundation
 import HTMLReader
 
+enum ImageSize: String {
+    case Small, Medium, Large
+}
+
+enum Format: String {
+    case JSON = "json"
+    case XML = "atom"
+}
+
 final class BingSearchAPIClient {
     
+    // MARK: Properties
+    var numberOfPhotos: Int = 3 {
+        didSet {
+            assert(numberOfPhotos <= 10, "Attempt to set numberOfPhotos on request to be larger than 10.")
+            if numberOfPhotos > 10 { numberOfPhotos = 10 }
+        }
+    }
+    var baseURL: NSURL  {
+        print("Inside of baseURL")
+        let urlString = "https://api.datamarket.azure.com/Bing/Search/v1/Composite?Sources=%27image%27&$top=\(numberOfPhotos)&$format=\(format.rawValue)"
+
+        if let imageSize = imageSize {
+            let imageSizeQuery = "&ImageFilters=%27Size%3A\(imageSize.rawValue)%27"
+            let urlStringWithImageSizeQuery = urlString + imageSizeQuery
+            return NSURL(string: urlStringWithImageSizeQuery)!
+        }
+        
+        return NSURL(string: urlString)!
+    }
+    var format: Format = .JSON
+    var imageSize: ImageSize? // Not required
     
+    // MARK: Initializers
+    init() {}
+    
+    convenience init(numberOfPhotos: Int, format: Format, imageSize: ImageSize?) {
+        self.init()
+        self.numberOfPhotos = numberOfPhotos
+        self.format  = format
+        self.imageSize = imageSize
+    }
+    
+}
+
+// MARK: Search Methods
+extension BingSearchAPIClient {
     
     
     
--- WhoSaidIt/MasterViewController.swift
+++ WhoSaidIt/MasterViewController.swift
@@ -9,12 +9,14 @@ class MasterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let searchClient = BingSearchAPIClient()
+        print(searchClient.baseURL)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 }
